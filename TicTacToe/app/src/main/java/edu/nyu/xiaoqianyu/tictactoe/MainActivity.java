75345e6b2646ff9e18e10294ad8429b2ff4b4dab
Xiaoqian Yu <xiaoqianyupt@gmail.com>
2015-04-02 19:32:47 -0400

    Welcome activity built OK
    main activity jump to game play activity

--- TicTacToe/app/src/main/java/edu/nyu/xiaoqianyu/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/edu/nyu/xiaoqianyu/tictactoe/MainActivity.java
@@ -1,17 +1,47 @@
 package edu.nyu.xiaoqianyu.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import edu.nyu.xiaoqianyu.tictactoe.activity.GamePlayActivity;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public static String PLAYER_VS_COM = "Player_vs_com";
+    public static String PLAYER1_VS_PLAYER2 = "Player1_vs_player2";
+    public static String CHOSEN_BUTTON = "Chosen_button";
+    private Button button1;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), GamePlayActivity.class);
+                intent.putExtra(CHOSEN_BUTTON, PLAYER_VS_COM);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), GamePlayActivity.class);
+                intent.putExtra(CHOSEN_BUTTON, PLAYER1_VS_PLAYER2);
+                startActivity(intent);
+            }
+        });
     }
 
 
