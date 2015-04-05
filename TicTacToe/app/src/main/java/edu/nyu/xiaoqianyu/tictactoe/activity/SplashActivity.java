e1b30beb8e54ff7aa48bc13881a8b53622574a3e
Xiaoqian Yu <xiaoqianyupt@gmail.com>
2015-04-05 16:00:29 -0400

    Add a splash screen

---
+++ TicTacToe/app/src/main/java/edu/nyu/xiaoqianyu/tictactoe/activity/SplashActivity.java
@@ -0,0 +1,38 @@
+package edu.nyu.xiaoqianyu.tictactoe.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import edu.nyu.xiaoqianyu.tictactoe.MainActivity;
+import edu.nyu.xiaoqianyu.tictactoe.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Finish the splash activity so it can't be returned to.
+                SplashActivity.this.finish();
+                // Create an Intent that will start the main activity.
+                Intent mainIntent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+            }
+        }, 2000);
+    }
+}
