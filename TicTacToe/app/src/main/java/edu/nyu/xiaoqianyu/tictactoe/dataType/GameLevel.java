a107ead7e99744657050e90658493fc4dd736cca
kkpatrick <xiaoqianyupt@gmail.com>
2015-10-13 19:17:56 -0400

    asdfa
    asdfads

--- TicTacToe/app/src/main/java/edu/nyu/xiaoqianyu/tictactoe/dataType/GameLevel.java
+++
@@ -1,9 +0,0 @@
-package edu.nyu.xiaoqianyu.tictactoe.dataType;
-
-/**
- * Created by abc on 4/3/15.
- */
-public enum GameLevel {
-    EASY,
-    HARD;
-}
--- TicTacToe/app/src/main/java/edu/nyu/xiaoqianyu/tictactoe/dataType/VsMode.java
+++
@@ -1,9 +0,0 @@
-package edu.nyu.xiaoqianyu.tictactoe.dataType;
-
-/**
- * Created by abc on 4/3/15.
- */
-public enum VsMode {
-    PLAYER_VS_PLAYER,
-    PLAYER_VS_COM;
-}
